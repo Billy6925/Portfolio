cf9f57ddf422df3be145e799b660f7cf4af68603
Billy Eskein <billyeskein@gmail.com>
2026-07-11 17:25:37 +0300

    New nav list item hover color

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,13 +1,14 @@
 import { Link,useLocation } from "react-router-dom"
+import "../App.css";
 
 export default function Navbar(){
     const location=useLocation();
 
     return(
         <nav className="navbar d-flex justify-content-end align-items-center p-1 border-bottom border-success mt-0">
-            {location.pathname !== "/" && <Link to="/" className="text-decoration-none text-success px-3">Home</Link>}
-            {location.pathname !== "/contact" && <Link to="/contact" className="text-decoration-none text-success">Contact Me</Link>}
-            {location.pathname !== "/projects" && <Link to="/projects" className="text-decoration-none text-success px-3">Projects</Link>}
+            {location.pathname !== "/" && <Link to="/" className="view text-decoration-none text-success px-3">Home</Link>}
+            {location.pathname !== "/contact" && <Link to="/contact" className="view text-decoration-none text-success">Contact Me</Link>}
+            {location.pathname !== "/projects" && <Link to="/projects" className="view text-decoration-none text-success px-3">Projects</Link>}
         </nav>
     )
 }
