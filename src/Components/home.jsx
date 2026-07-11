6438637556319d97e34eeef164bd2aab61da6748
Billy Eskein <billyeskein@gmail.com>
2026-07-11 16:06:07 +0300

    Updated home page

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Home(){
     return(
         <div className="container py-4">
-            <div className="home px-5 row align-items-center">
-                <div className="col-lg-7">
+            <div className="home px-3 px-lg-5 row align-items-center">
+                <div className="col-8 col-lg-7">
                     <h3 className="text-success fw-normal">Hi, I'm Billy Eskein</h3>
                     <h4 className="text-success">Full-stack Software Developer.</h4>
                     <p className="text-secondary">I build full-stack applications with React and Flask.</p>
@@ -17,14 +17,14 @@ export default function Home(){
                         <li>✓ Integrate payment solutions such as M-PESA</li>
                     </ul>
 
-                    <section className="justify-content-between">
+                    <section className="d-flex flex-column flex-sm-row gap-2 mt-4">
                         <a href="/resume/resume.pdf" target="_blank" rel="noopener noreferrer" className="btn btn-success mx-2">Download Resume</a>
                         <Link to="/contact" className="btn btn-secondary m-2">Contact Me</Link>
                     </section>
                 </div>
 
-                <div className="col-lg-5 text-center">
-                    <img src="/videos/billy.jpeg" alt="Billy Eskein" style={{width:"210px"}} className="border border-success rounded-3 shadow-lg"/>
+                <div className="col-4 col-lg-5 text-center">
+                    <img src="/videos/billy.jpeg" alt="Billy Eskein" style={{maxWidth:"210px", width:"100%"}} className="img-fluid border border-success rounded-3 shadow-lg"/>
                 </div>
 
             </div>
